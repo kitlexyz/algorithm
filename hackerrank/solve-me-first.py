a73eac3fd78ea4b041d7df900001589343636331
kitle <kitle.xyz@gmail.com>
2020-07-25 19:13:50 +0900

    Add solution for solve-me-first

---
+++ hackerrank/solve-me-first.py
@@ -0,0 +1,16 @@
+"""
+source: https://www.hackerrank.com/challenges/solve-me-first/problem
+solution: use +
+
+Complete the function solveMeFirst to compute the sum of two integers.
+"""
+
+
+def solveMeFirst(a,b):
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
